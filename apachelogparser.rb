dcc0aad1a59554a45b9b829118715d6cd88e15e0
Aravind G V <aravind.gv@gmail.com>
2014-05-12 09:47:55 +0530

    .

--- apachelogparser.rb
+++ apachelogparser.rb
@@ -1,15 +1,16 @@
 require "rubygems"
 require "apachelogregex"
-# This is the log line you want to parse
-line = '87.18.183.252 - - [13/Aug/2008:00:50:49 -0700] "GET /blog/index.xml HTTP/1.1" 302 527 "-" "Feedreader 3.13 (Powered by Newsbrain)"'
+# This is the log line/File you want to parse
+# uncomment if u want to test one line. and comment log file 3 lines
+#line = '87.18.183.252 - - [13/Aug/2008:00:50:49 -0700] "GET /blog/index.xml HTTP/1.1" 302 527 "-" "Feedreader 3.13 (Powered by Newsbrain)"'
+line = File.open(/var/log/apache.log).collect do | line |
+	valuses = parser.parse(line)
+end
 
 # Define the log file format.
 # This information is defined in you Apache log file
 # with the LogFormat directive
 format = '%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"'
-line = File.open(/var/log/apache.log).collect do | line |
-	valuses = parser.parse(line)
-end
 
 parser = ApacheLogRegex.new(format)
 valuses = parser.parse(line)
